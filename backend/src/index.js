26cf01cbba4551022383fdf539a063d3a3ec25a8
Paul Andrei Mendiola <mendiolapaulandrei20@gmail.com>
2026-07-05 16:55:53 +0800

    update dependencies and enhance server setup
    - Updated @clerk/express to version 2.1.35 in package.json and package-lock.json
    - Updated @clerk/backend to version 3.10.0 in package-lock.json
    - Updated @clerk/shared to version 4.23.0 in package-lock.json
    - Updated @tanstack/query-core to version 5.101.2 in package-lock.json
    - Added connectDB function call in server startup for database connection
    - Improved health check endpoint response structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import cors from 'cors'
 import "dotenv/config"
+import { connectDB } from './lib/db.js'
+
+import {clerkMiddleware} from '@clerk/express'
 
 const app = express()
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL
 
-app.listen(PORT, () => console.log(`Server is running and running on port ${PORT}`))
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL, credentials: true}))
+app.use(clerkMiddleware())
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ok: true})
+})
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`Server is running and running on port ${PORT}`)
+})
